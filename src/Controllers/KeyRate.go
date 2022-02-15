ea04890593e1a8f9b01c0b372e09227d20ec2904
fomin@kodeks.ru <fomin@kodeks.ru>
2022-02-16 02:06:36 +0300

    #7 Постраничный вывод

--- src/Controllers/KeyRate.go
+++ src/Controllers/KeyRate.go
@@ -2,9 +2,10 @@ package Controllers
 
 import (
 	"key-rate-api/src/client"
-
 	"log"
+	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,10 @@ import (
 func KeyRate(c *gin.Context) {
 	layout := "2006-01-02"
 	fromDate := c.DefaultQuery("fromDate", time.Now().Format(layout))
+	currentPageNumber := c.DefaultQuery("page", "1")
+	currentPageNumberInt, _ := strconv.Atoi(currentPageNumber)
+	perPageNumber := c.DefaultQuery("per_page", "3")
+	PerPage, _ := strconv.Atoi(perPageNumber)
 
 	t, err := time.Parse(layout, fromDate)
 	if err != nil {
@@ -33,7 +38,26 @@ func KeyRate(c *gin.Context) {
 		return
 	}
 
+	dataKeyRates := data.Body.Response.Result.Rows[0].KeyRates
+	total := len(dataKeyRates)
+	totalPagesFloat := float64(total / PerPage)
+	totalPages := int(math.Ceil(totalPagesFloat))
+
+	result := make([]client.KeyRates, 0)
+	startIndex := currentPageNumberInt * PerPage
+	endIndex := startIndex + PerPage
+
+	for i := startIndex; i < endIndex; i++ {
+		if currentPageNumberInt <= totalPages && i <= total-1 {
+			result = append(result, dataKeyRates[i])
+		}
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"data": data.Body.Response.Result.Rows[0].KeyRates,
+		"data":         result,
+		"total":        total,
+		"current_page": currentPageNumberInt,
+		"per_page":     PerPage,
+		"total_pages":  totalPages,
 	})
 }
